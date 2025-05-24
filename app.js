ff3ba400d287d5801b0c996fc939410d511ad5a4
Rajasah2000 <rajasah30030@gmail.com>
2025-05-24 08:11:10 +0530

    up

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup for the app
+// view engine setup for the appp
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
